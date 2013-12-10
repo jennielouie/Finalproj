f9014992601b7663521338e24b1f9147e0efc5fc
Jennie Louie <jennie.louie@gmail.com>
2013-12-09 21:49:47 -0800

    Revised config/application.rb to fix precompile heroku error

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe ProjectsController do
-
-end
+# describe ProjectsController do
+#   describe "GET index" do
+#     it "should assign @projects" do
+#       project = Project.create()
+#       get :index
+#       expect(assigns(:projects)). to eq([project])
+#     end
+#   end
+# end
