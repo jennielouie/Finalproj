1ce2fce0ee3dd32845fa094c66e440c4eda980e7
Jennie Louie <jennie.louie@gmail.com>
2013-12-10 18:36:37 -0800

    commented rspec tests for heroku debug

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,13 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe Project do
+# describe Project do
+#   it "is valid with a project title" do
+#     project = Project.new(projtitle: "Mittens")
+#     expect(project).to be_valid
+#   end
 
+#   it "is not valid without a project title" do
+#     expect(Project.new(projtitle: nil)).to have(1).errors_on(:projtitle)
+#   end
 
-end
+# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,22 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe User do
-  it "is valid with a name and email address" do
-    user = User.new(name: 'Jennie', email: 'foo@foo.com')
-    expect(user).to be_valid
-  end
-  it "is invalid without a name" do
-    expect(User.new(name: nil)).to have(1).errors_on(:name)
-  end
+# describe User do
+#   it "is valid with a name and email address" do
+#     user = User.new(name: 'Jennie', email: 'foo@foo.com')
+#     expect(user).to be_valid
+#   end
+#   it "is invalid without a name" do
+#     expect(User.new(name: nil)).to have(1).errors_on(:name)
+#   end
 
-  it "is invalid without an email address" do
-    expect(User.new(email:nil)).to have(1).errors_on(:email)
-  end
+#   it "is invalid without an email address" do
+#     expect(User.new(email:nil)).to have(1).errors_on(:email)
+#   end
 
-  it "is invalid with a duplicated email address" do
-    User.create(name: "Jennie", email: "foo@foo.com")
-    user = User.new(name: 'Mike', email: "foo@foo.com")
-    expect(user).to have(1).errors_on(:email)
-  end
+#   it "is invalid with a duplicated email address" do
+#     User.create(name: "Jennie", email: "foo@foo.com")
+#     user = User.new(name: 'Mike', email: "foo@foo.com")
+#     expect(user).to have(1).errors_on(:email)
+#   end
 
-end
+# end
