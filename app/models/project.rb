55ef7d1374b37ab0d01603860d22483e86d5c582
Jennie Louie <jennie.louie@gmail.com>
2013-12-10 18:49:20 -0800

    added back file upload and rspec tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,8 @@
 class Project < ActiveRecord::Base
-  attr_accessible :projtitle, :author, :notes
-  # , :patternFile
+  attr_accessible :projtitle, :author, :notes, :patternFile
   has_many :users, through: :project_users
 
-  # has_attached_file :patternFile
+  has_attached_file :patternFile
 
   validates :projtitle, presence: true
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,15 +1,15 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe ProjectsController do
-#   describe "GET index" do
-#     it "renders the index view" do
-#       expect(response).to render_template :index
-#     end
-#   end
+describe ProjectsController do
+  describe "GET index" do
+    it "renders the index view" do
+      expect(response).to render_template :index
+    end
+  end
 
-#   describe "GET #new" do
-#     it "renders the new user view" do
-#       expect(response).to render_template :new
-#     end
-#   end
-# end
+  describe "GET #new" do
+    it "renders the new user view" do
+      expect(response).to render_template :new
+    end
+  end
+end
