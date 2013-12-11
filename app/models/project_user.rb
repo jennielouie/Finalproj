92f9cc6e3d7019aa0d0b732d35f04c771218843d
Jennie Louie <jennie.louie@gmail.com>
2013-12-10 19:46:41 -0800

    commit before merge

--- app/models/project_user.rb
+++
@@ -1,6 +0,0 @@
-class ProjectUser < ActiveRecord::Base
-  attr_accessible :dueDate, :user_id, :project_id
-
-  belongs_to: user
-  belongs_to: project
-end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,12 +3,20 @@ require 'spec_helper'
 describe ProjectsController do
   describe "GET index" do
     it "renders the index view" do
+      get :index
       expect(response).to render_template :index
+    end
+
+    it "should assign @projects" do
+      project = Project.create(projtitle: 'Shorts')
+      get :index
+      expect(assigns(:projects)).to eq([project])
     end
   end
 
   describe "GET #new" do
-    it "renders the new user view" do
+    it "renders the new project view" do
+      get :new
       expect(response).to render_template :new
     end
   end
